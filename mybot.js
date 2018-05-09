dc4acf90e5eccb8420967d4a2cfeb41cef4c2184
Cameron Wright <cameronwright@u.boisestate.edu>
2018-05-08 19:30:22 -0600

    Added config file and argument checking for commands.

--- mybot.js
+++ mybot.js
@@ -1,21 +1,35 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = process.argv[2];
-
-if(token == null){
-    process.stdout.write("$node mybot.js <token>");
-    process.exit(0);
-}
-//process.stdout.write(token);
+const config = require("./config.json");
+const fs = require("fs");
 
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+    // Exit and stop if it's not there
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case "ping":
+            message.channel.send('Pong!');
+            break;
+        case "foo":
+            message.channel.send("bar!");
+            break;
+        case "prefix":
+            // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+            let newPrefix = message.content.split(" ").slice(1, 2)[0];
+            // change the configuration in memory
+            config.prefix = newPrefix;
+            // Now we have to save the file.
+            fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+            break;
+    }
 });
 
-client.login(token);
+client.login(config.token);
